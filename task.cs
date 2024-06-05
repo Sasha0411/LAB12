00a21b67a29b90a7328739a9ed8ed64f34c5542d
Sasha0411 <162753245+Sasha0411@users.noreply.github.com>
2024-06-05 14:16:56 +0300

    Create task.cs

---
+++ task.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB12
+{
+    internal class Program
+    {
+        static double DegToRad(double degrees)
+        {
+            const double pi = 3.14; // Константа π
+            return degrees * pi / 180; // Перетворення з градусів в радіани
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Горкун Олександр");
+                Console.Write("Введіть величину кута в градусах (або 0 для виходу): ");
+                double degrees = double.Parse(Console.ReadLine());
+
+                if (degrees == 0)
+                {
+                    break; // Вихід з циклу, якщо введено 0
+                }
+
+                double radians = DegToRad(degrees);
+                Console.WriteLine($"{degrees} градусів дорівнює {radians:F2} радіанам."); // Вивід результату
+            }
+
+            Console.WriteLine("Програму завершено.");
+        }
+    }
